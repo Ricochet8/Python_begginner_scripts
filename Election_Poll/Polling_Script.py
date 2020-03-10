1285e6bfe1e3a126b25c855a06d4b37c4efd5bfd
ricochet <eashaw8@gmail.com>
2020-03-10 15:11:46 -0500

    count

--- Election_Poll/Polling_Script.py
+++ Election_Poll/Polling_Script.py
@@ -3,7 +3,18 @@ import csv
 
 from csv import DictReader
 
-csvpath = os.path.join('..', 'election_data.csv')
+csvpath = os.path.join('election_data.csv')
+
+def print_count(election_data):
+    voter_id = str(election_data[0])
+    county = str(election_data[1])
+    candidate = str(election_data[2])
+
+    total_votes = election_data.voter_id.count()
+    
+
+    print('str{}'.format(total_votes))
+
 
 with open(csvpath, newline='') as csvfile:
 
@@ -11,16 +22,17 @@ with open(csvpath, newline='') as csvfile:
 
   #  print(csvreader)
 
-    csv_header = next(csvreader)
-    print("CSV Header: {}".format(csv_header))
+    header = next(csvreader)
+    
+    for row in csvreader:
+        print(print_count)
 
-  #  for row in csvreader:
-  #     print(row)
+   
 
 
-with open (csvfile):
-    VoterID = [row[0] for row in DictReader(csvfile)]
-    print(VoterID)
+
+
+
 
 khan_count = 0
 correy_count = 0
@@ -29,11 +41,7 @@ otooley_count = 0
 
 
 
-# for person in csvfile[2]:
-#     if person == 'Khan':
-#         khan_count[person] += 1
-    
-# print('Khan Count: {}'.format(khan_count))
+
 
 
 
