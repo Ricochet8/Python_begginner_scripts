bddfe3d480fea9b011734cf41e1db322708a16e5
ricochet <eashaw8@gmail.com>
2020-03-11 14:22:59 -0500

    commit

--- Election_Poll/Polling_Script.py
+++ Election_Poll/Polling_Script.py
@@ -5,16 +5,6 @@ from csv import DictReader
 
 csvpath = os.path.join('election_data.csv')
 
-def print_count(election_data):
-    voter_id = str(election_data[0])
-    county = str(election_data[1])
-    candidate = str(election_data[2])
-
-    total_votes = election_data.voter_id.count()
-    
-
-    print('str{}'.format(total_votes))
-
 
 with open(csvpath, newline='') as csvfile:
 
@@ -22,34 +12,21 @@ with open(csvpath, newline='') as csvfile:
 
   #  print(csvreader)
 
-    header = next(csvreader)
-    
-    for row in csvreader:
-        print(print_count)
+    #header = next(csvreader)
 
-   
+    def print_count(election_data):
+      voter_id = str(election_data[0])
+      county = str(election_data[1])
+      candidate = str(election_data[2])
 
+      total_votes = election_data.voter_id.count()
 
+      return('total votes: {}'.format(total_votes))
 
-
+    print_count
 
 
 khan_count = 0
 correy_count = 0
 li_count = 0
 otooley_count = 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
